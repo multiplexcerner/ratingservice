e33c386aa2124906df827b0da9ac7462eac8acbc
KN073805 <Krishna.Nayak@cerner.com>
2020-01-23 20:40:46 +0530

    ItemRatingResource.java

--- src/main/java/org/cerner/multiplex/packageinfo/resource/ItemRatingResource.java
+++ src/main/java/org/cerner/multiplex/packageinfo/resource/ItemRatingResource.java
@@ -8,7 +8,14 @@ import org.cerner.multiplex.packageinfo.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -30,6 +37,7 @@ public class ItemRatingResource {
     {
         int key = Integer.parseInt(id);
         return ResponseEntity.status(HttpStatus.OK).body(hospitalDAO.get(key));
+
     }
 
     @PutMapping("/rating")
